5a690eb68c012e91d9aab0aa5eca2de4d6d476d4
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-02-29 16:28:47 -0300

    Add files via upload

---
+++ MapRelacional/src/test/java/com/maprelacional/MapRelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maprelacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapRelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
